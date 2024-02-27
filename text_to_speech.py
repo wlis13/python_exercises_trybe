255336396c91ce3c5bf2ba642c1773503a1f9421
wlisses <wlissesfernando285@gmail.com>
2024-02-27 19:17:19 -0300

    all exercises

---
+++ text_to_speech.py
@@ -0,0 +1,37 @@
+from random import randint
+
+DIGITS_MAP = {
+    0: "zero",
+    1: "um",
+    2: "dois",
+    3: "três",
+    4: "quatro",
+    5: "cinco",
+    6: "seis",
+    7: "sete",
+    8: "oito",
+    9: "nove",
+}
+
+
+def generate_int_description(integer):
+    """Transformar digitos de número em texto PT-BR"""
+    digits = list(str(integer))
+
+    description = f"{DIGITS_MAP.get(int(digits[0]))}"
+    for digit in digits[1:]:
+        description += f" {DIGITS_MAP.get(int(digit))}"
+
+    return description
+
+
+def main():
+    integer = randint(10000, 99999)
+
+    description = generate_int_description(integer)
+
+    print(f"Descrição do número{integer}: {description}")
+
+
+if __name__ == "__main__":
+    main()
