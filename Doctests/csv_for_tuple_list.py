e5099310748090572c339504bd5b7f300422e9b8
wlisses <wlissesfernando285@gmail.com>
2024-03-01 18:07:27 -0300

    testing functions

--- Doctests/csv_for_tuple_list.py
+++ Doctests/csv_for_tuple_list.py
@@ -1,4 +1,5 @@
 import csv
+from typing import List, Dict
 
 
 def csv_for_tuple_list(path_file):
@@ -13,4 +14,42 @@ def csv_for_tuple_list(path_file):
     print(tuple_list)
 
 
-csv_for_tuple_list("text.csv")
+# csv_for_tuple_list("text.csv")
+
+
+def csv_for_list(path_file):
+    with open(path_file, "r") as file:
+        content_file = csv.reader(file)
+        result = []
+        characters_word = []
+
+        for line in content_file:
+            print(line)
+            for word in line:
+                result.append(word)
+                for cha in word:
+                    characters_word.append(cha)
+
+        print(f"words: ${result}")
+        print(f"caracteres: ${characters_word}")
+
+
+def prime_numbers(numbers: List[int]) -> Dict[str, str]:
+    result = {}
+    for num in numbers:
+        if num <= 1:
+            result[f"{num}"] = "Não é primo"
+        else:
+            count = 0
+            for tn in range(2, int(num**0.5) + 1):
+                if num % tn == 0:
+                    count += 1
+            if count > 0:
+                result[f"{num}"] = "Não é primo."
+            else:
+                result[f"{num}"] = "É primo."
+
+    print(result)
+
+
+prime_numbers([1, 2, 6, 4, 8, 9, 11, 15, 16, 23, 45])
