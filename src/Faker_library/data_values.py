4f139985a5a04b6e5946c361f26c40004626efaf
wlisses <wlissesfernando285@gmail.com>
2024-03-07 15:19:13 -0300

    added united states lacale verification

--- src/Faker_library/data_values.py
+++ src/Faker_library/data_values.py
@@ -1,18 +1,30 @@
 import random
 
-dates = {
+dates_locales = {
     "pt-BR": {
-        "first_part": random.choice(["AYT", "SAW", "NYG", "PKI"]),
+        "first_part": random.choice(["BRA", "SAW", "NYG", "PKI"]),
         "second_part": random.choice(["7Y45", "2L54", "0N26", "1U20"]),
     },
     "es-AR": {
-        "first_part": random.choice(["AA", "AB", "AC", "AD", "AE"]),
+        "first_part": random.choice(["AR", "AB", "AC", "AD", "AE"]),
         "half_part": random.randint(1000, 9999),
         "second_part": random.choice(["ES", "RS", "LS"]),
     },
     "fr-FR": {
         "first_part": random.randint(1000, 9999),
-        "half_part": random.choice(["AA", "AB", "AC", "AD", "AE"]),
+        "half_part": random.choice(["FR", "AB", "AC", "AD", "AE"]),
         "second_part": random.randint(10, 99),
+    },
+    "en-UA": {
+        "first_part": random.choice(["AUS", "TRS", "CXS", "VZX"]),
+        "second_part": random.randint(100, 999),
+    },
+    "de-DE": {
+        "first_part": random.choice(["ALM", "IUB", "MNC", "MMC"]),
+        "second_part": random.randint(100, 999),
+    },
+    "en-US": {
+        "first_part": random.choice(["ALM", "IUB", "MNC", "MMC"]),
+        "second_part": random.randint(100, 999),
     },
 }
--- src/Faker_library/fake_names.py
+++ src/Faker_library/fake_names.py
@@ -1,28 +1,30 @@
 from faker import Faker
 from faker.providers import BaseProvider
-from data_values import dates
+from data_values import dates_locales
 
 fake = Faker()
 
-fake = Faker(locale="es-AR")
+fake = Faker(locale="en-US")
 
 
 class MyProvider(BaseProvider):
     def car_license_plate(self) -> str:
-        for key in dates.keys():
+        for key in dates_locales.keys():
             if fake.locales[0].replace("_", "-") == key:
-                if "half_part" in dates[key]:
+                if "half_part" in dates_locales[key]:
 
-                    first_element = dates[key]["first_part"]
-                    half_element = dates[key]["half_part"]
-                    second_element = dates[key]["second_part"]
+                    first_element = dates_locales[key]["first_part"]
+                    half_element = dates_locales[key]["half_part"]
+                    second_element = dates_locales[key]["second_part"]
 
                     return f"{first_element} {half_element} {second_element}"
                 else:
-                    first_element = dates[key]["first_part"]
-                    second_element = dates[key]["second_part"]
-
-                    return f"{first_element}-{second_element}"
+                    first_element = dates_locales[key]["first_part"]
+                    second_element = dates_locales[key]["second_part"]
+                    if dates_locales[key] == "en-US":
+                        return f"{first_element}{second_element}"
+                    else:
+                        return f"{first_element}-{second_element}"
 
 
 fake.add_provider(MyProvider)
