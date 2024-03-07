a3cc081dd7447865fd898f4423563d7c98dd3044
wlisses <wlissesfernando285@gmail.com>
2024-03-07 14:40:36 -0300

    created data file and added in car_license_plate function a veryfication for to select locale and to return correct values

---
+++ src/Faker_library/data_values.py
@@ -0,0 +1,11 @@
+dates = {
+    "pt-BR": {
+        "first_part": ["AYT", "SAW", "NYG", "PKI"],
+        "second_part": ["7Y45", "2L54", "0N26", "1U20"],
+    },
+    "es-AR": {
+        "first_part": ["AA", "AB", "AC", "AD", "AE"],
+        "half_part": "",
+        "second_part": ["ES", "RS", "LS"],
+    },
+}
---
+++ src/Faker_library/fake_names.py
@@ -0,0 +1,39 @@
+from faker import Faker
+from faker.providers import BaseProvider
+from data_values import dates
+
+fake = Faker()
+
+fake = Faker(locale="pt-BR")
+
+
+class MyProvider(BaseProvider):
+    def car_license_plate(self) -> str:
+        for key in dates.keys():
+            if fake.locales[0].replace("_", "-") == key:
+                if "half_part" in dates[key]:
+                    first_element = self.random_element(
+                        dates[key]["first_part"]
+                    )
+                    half_element = self.random_number(4)
+                    second_element = self.random_element(
+                        dates[key]["second_part"]
+                    )
+                    return f"{first_element} {half_element} {second_element}"
+                else:
+                    first_element = self.random_element(
+                        dates[key]["first_part"]
+                    )
+                    second_element = self.random_element(
+                        dates[key]["second_part"]
+                    )
+                    return f"{first_element}-{second_element}"
+
+
+fake.add_provider(MyProvider)
+
+print(fake.last_name())
+print(fake.email())
+print(fake.password())
+print(fake.url())
+print(fake.car_license_plate())
